b64453f8ef4b2830c6efe948001892a888e696d3
Void-Individual <otayomitv@gmail.com>
2023-11-08 21:33:21 +0100

    edited shell

---
+++ lstest.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * main - execute ls as many times
+*/
--- shell.c
+++ shell.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	char *cmd = NULL, *args;
+	char *cmd = NULL;
 	char *argv[] = {NULL};
 	int  status;
 	size_t read, count = 0;
@@ -31,15 +31,16 @@ int main(void)
 			return (-1);
 		}
 		cmd[read - 1] = '\0';
+		if (cmd[0] == "exit")
+			break;
 
 		child = fork();
 		if (child == -1)
 			printf("Oops, forking error");
 		else if (child == 0)
 		{
-			args = cmd[0];
 			argv[0] = cmd;
-			if (execve(args, argv, NULL) == -1)
+			if (execve(cmd, argv, NULL) == -1)
 			{
 				printf("Execve error\n");
 				return (-1);
