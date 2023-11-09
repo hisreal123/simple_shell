f567f8dbe89f7f775af32367b54b45643bdcf870
Void-Individual <otayomitv@gmail.com>
2023-11-09 22:34:39 +0100

    non interactive

--- 0-shell.c
+++ 0-shell.c
@@ -11,13 +11,14 @@
 int main(int argc, char *argv[], char *envp[])
 {
 	int check = 0, loop = 1;
+	char *cmd = argv[1];
+	char*args[] = {cmd, NULL};
 
 	if (argc > 1)
 	{
-		if (execve(argv[argc], argv, envp) == -1)
-			printf("%s: 1: %s: not found\n", argv[argc], *argv);
+		if (execve(cmd, args, envp) == -1)
+			printf("%s: 1: %s: not found", argv[argc - 1], cmd);
 	}
-
 	else
 	{
 			for (;; loop++)
